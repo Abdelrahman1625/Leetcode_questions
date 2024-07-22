b600ef53423c0fcb4d797e2a7b6f2287075cd52d
Abdelrahman <95689929+Abdelrahman1625@users.noreply.github.com>
2024-07-22 04:24:58 +0300

    [540] [Time Beats: 58.51%] [Memory Beats: 8.51%] - LeetPush

---
+++ 540-Single-Element-in-a-Sorted-Array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int SingleNonDuplicate(int[] nums) {
+        for (int i = 0; i < nums.Length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.Length - 1];
+    }
+}
