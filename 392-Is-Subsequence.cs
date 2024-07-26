66f7aa1fd40dbc302c3179a655bc1059510b4136
Abdelrahman <95689929+Abdelrahman1625@users.noreply.github.com>
2024-07-26 16:58:36 +0300

    [392] [Time Beats: 8.38%] [Memory Beats: 15.85%] - LeetPush

--- 392-Is-Subsequence.cs
+++ 392-Is-Subsequence.cs
@@ -1,12 +1,14 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-        int Si = 0, Ti = 0;
-        while (Si < s.Length && Ti < t.Length) {
-            if (s[Si] == t[Ti]) {
-                Si++;
+        int sx = 0 , ty = 0;
+        while (sx < s.Length && ty < t.Length)
+        {
+            if(s[sx] == t[ty])
+            {
+                sx++;
             }
-            Ti++;
+            ty++;
         }
-        return Si == s.Length;
+        return sx == s.Length;  
     }
 }
