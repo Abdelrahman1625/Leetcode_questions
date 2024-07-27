33fe9edac0449025cb814a58832d40013f9527b6
Abdelrahman <95689929+Abdelrahman1625@users.noreply.github.com>
2024-07-27 22:43:33 +0300

    [15] [Time Beats: 42.49%] [Memory Beats: 86.35%] - LeetPush

--- 15-3Sum.cs
+++ 15-3Sum.cs
@@ -5,29 +5,24 @@ public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
         Array.Sort(nums);
         var result = new List<IList<int>>();
-
-        for (int i = 0; i < nums.Length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
+        for(int i = 0; i < nums.Length - 2; i++)
+        {
+            if((i > 0 )&& (nums[i] == nums[i - 1])) continue;
             int left = i + 1;
             int right = nums.Length - 1;
-
-            while (left < right) {
+            while (left < right)
+            {
                 int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
+                if(sum == 0)
+                {
                     result.Add(new List<int> { nums[i], nums[left], nums[right] });
-                    left++;
-                    right--;
-
-                    // Skip duplicate elements
+                    left ++;
+                    right --;
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
                 }
+                else if (sum < 0) left++;
+                else right--;
             }
         }
         return result;
