86e703d93721f9954fd7a387071cebe1053f58e8
Abdelrahman <95689929+Abdelrahman1625@users.noreply.github.com>
2024-08-19 17:06:20 +0300

    [735] [Time Beats: 78.53%] [Memory Beats: 8.97%] - LeetPush

--- 735-Asteroid-Collision.cs
+++ 735-Asteroid-Collision.cs
@@ -7,12 +7,12 @@ public class Solution {
             bool destroy = false;
             while (stack.Count > 0 && asteroid < 0 && stack.Peek() > 0)
             {
-                if (stack.Peek() < -asteroid)
+                if (stack.Peek() < Math.Abs(asteroid))
                 {
                     stack.Pop();
                     continue;
                 }
-                else if (stack.Peek() == -asteroid)
+                else if (stack.Peek() == Math.Abs(asteroid))
                 {
                     stack.Pop();
                 }
