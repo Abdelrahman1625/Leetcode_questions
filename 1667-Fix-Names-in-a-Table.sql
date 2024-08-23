61933c5beee9791a7d8e76c550ca1c0b8f705628
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-08-23 20:05:36 +0300

    [1667] [Time Beats: 5.00%] - LeetPush

---
+++ 1667-Fix-Names-in-a-Table.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    concat(upper(substr(name,1,1)), lower(substr(name,2,length(name)))) AS name
+FROM
+    users
+ORDER BY
+    user_id;
