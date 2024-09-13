ebbfd614177eb61017b3d8b13010b71e389de492
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-09-13 16:40:44 +0300

    [54] [Time Beats: 26.03%] [Memory Beats: 85.13%] - LeetPush

--- 54-Spiral-Matrix.cs
+++ 54-Spiral-Matrix.cs
@@ -2,26 +2,26 @@ public class Solution {
     public IList<int> SpiralOrder(int[][] matrix) {
         var list = new List<int>();
         var n = matrix.Length;
-        int top = 0, bottom = n - 1, left = 0, right = matrix[0].Length - 1;
+        int top = 0, left = 0, bottom = n - 1, right = matrix[0].Length - 1;
         while(true){
-            for (int i = left; i <= right; i++)
+            for(int i = left; i <= right; i++)
                 list.Add(matrix[top][i]);
             top++;
             if (top > bottom || left > right) break;
 
-            for (int i = top; i <= bottom; i++)
+            for(int i = top; i <= bottom; i++)
                 list.Add(matrix[i][right]);
             right--;
             if (top > bottom || left > right) break;
-            
-            for (int i = right; i >= left; i--)
+
+            for(int i = right; i >= left; i--)
                 list.Add(matrix[bottom][i]);
             bottom--;
             if (top > bottom || left > right) break;
 
-            for (int i = bottom; i >= top; i--)
+            for(int i = bottom; i >= top; i--)
                 list.Add(matrix[i][left]);
-            left++;            
+            left++;
             if (top > bottom || left > right) break;
         }
         return list;
