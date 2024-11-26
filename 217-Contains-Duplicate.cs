3881b69d235f5b428cadd3568b66f0e43faa066d
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-11-26 21:43:31 +0200

    [217] [Time Beats: 16.65%] [Memory Beats: 60.12%] - LeetPush

--- 217-Contains-Duplicate.cs
+++ 217-Contains-Duplicate.cs
@@ -1,15 +1,10 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.Length; i++)
-        {
-            for (int j = i + 1; j < nums.Length; j++)
-            {
-                if (nums[i] == nums[j])
-                {
-                    return true;
-                }
-            }
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length - 1; i++) {
+            if (nums[i] == nums[i+1]) 
+                return true;
         }
-        return false;
+        return false; 
     }
 }
