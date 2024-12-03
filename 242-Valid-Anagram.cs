6821cead9835cddf566542c95e4e8c70c97e1d37
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-12-03 18:40:22 +0200

    [242] [Time Beats: 14.74%] [Memory Beats: 10.97%] - LeetPush

--- 242-Valid-Anagram.cs
+++ 242-Valid-Anagram.cs
@@ -1,23 +1,12 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
         if(s.Length != t.Length)
-        {
             return false;
-        }
-        int[] charCountS = new int[26];
-        int[] charCountT = new int[26];
+        char[] sArray = s.ToCharArray();
+        char[] tArray = t.ToCharArray();
 
-        for (int i = 0; i < s.Length; i++) {
-            charCountS[s[i] - 'a']++;
-            charCountT[t[i] - 'a']++;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (charCountS[i] != charCountT[i]) 
-            {
-                return false;
-            }
-        }
-        return true;
+        Array.Sort(sArray);
+        Array.Sort(tArray);
+        return new string(sArray) == new string(tArray);
     }
 }
