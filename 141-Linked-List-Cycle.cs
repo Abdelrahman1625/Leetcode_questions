2dbbaabf30c2c81727b4f1d6c21d4f6a43d5d3fe
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-12-11 11:30:13 +0200

    [141] [Time Beats: 9.47%] [Memory Beats: 50.20%] - LeetPush

---
+++ 141-Linked-List-Cycle.cs
@@ -0,0 +1,22 @@
+// public class ListNode {
+//     public int val;
+//     public ListNode next;
+//     public ListNode(int val = 0, ListNode next = null) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow_pointer = head, fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
