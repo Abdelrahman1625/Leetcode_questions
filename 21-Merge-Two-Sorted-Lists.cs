b6d217a20ae45457b9d590ab3a02124a619f3c92
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2024-12-11 11:05:30 +0200

    [21] [Time Beats: 5.64%] [Memory Beats: 98.26%] - LeetPush

---
+++ 21-Merge-Two-Sorted-Lists.cs
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode.Sort(list1);
+        ListNode.Sort(list2);
+        ListNode cur = null;
+    }
+}
